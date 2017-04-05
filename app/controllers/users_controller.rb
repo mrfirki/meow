52905f5eb201bf9f55e65a33bf5311f1ec012fe6
mrfirki <mrfikri94@gmail.com>
2017-04-05 18:17:09 +0800

    added login feature

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < Clearance::UsersController
+
+	def index
+	  @users = User.all  
+	end  
+
+	def show
+	  @user = User.find(params[:id])
+	 end
+
+	def edit
+	  	@user = User.find(params[:id])
+	end
+
+	def update
+  		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success] = "update success!!"
+			render :show
+		else
+			flash[:danger] = "update fail!!"
+			render 	:edit
+		end	
+  	end
+
+	def delete
+	 	@user = User.find_by(params[:id])
+	  	@user.destroy
+	  	redirect_to '/'
+	end	
+
+	private
+
+	  def user_params
+	    params.require(:user).permit(:name, :email, :password, :gender, :country)
+	  end
+end
