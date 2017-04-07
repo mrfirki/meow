5c50e5dbf854093a053db980c01d088cfb655f81
cySheng <cyusheng93@gmail.com>
2017-04-07 12:31:51 +0800

    moved home controller to twitter controller and deleted home controller

--- app/controllers/home_controller.rb
+++
@@ -1,3 +0,0 @@
-class HomeController < ApplicationController
-	$cat = "849570458475175936"
-end
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,4 +1,5 @@
 class TwitterController < ApplicationController
   def show
+  	$cat = "849570458475175936"
   end
 end
