a9656fc4340b1385891037209e7634907a19c67f
mrfirki <mrfikri94@gmail.com>
2017-04-10 15:50:26 +0800

    integrated youtube to carousel and set root to cat#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'cat/index'
+  root 'cat#index'
   get 'twitter/index'
   get 'tumblr/index'
-
-	root 'welcome#index'
 
 	resources :passwords, controller: "clearance/passwords", only: [:create, :new]
 	resource :session, controller: "clearance/sessions", only: [:create]
